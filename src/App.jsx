335a24e0e6350f75f8ebfb7cd2e8fc9ac10c6ec3
Nessa <nessaprogramming@gmail.com>
2024-10-09 13:21:39 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Button } from "@material-tailwind/react";
+
+export default function Example() {
+  return (
+    <div className="container py-10 px-10 mx-0 min-w-full flex flex-col items-center">
+      <Button>Button</Button>
+    </div>
+  );
+}
